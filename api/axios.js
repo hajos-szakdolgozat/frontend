f24333dcd6d024e7c1e5679f891a2ae25d894016
GyReka09 <gy.reka09@gmail.com>
2025-12-29 22:17:31 +0100

    axios + react-router-dom + auth context

---
+++ api/axios.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+export const httpClient = axios.create({
+  baseURL: "http://127.0.0.1:8000/",
+  withCredentials: true,
+});
+
+httpClient.interceptors.request.use(
+  (config) => {
+    const token = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("XSRF-TOKEN="))
+      ?.split("=")[1];
+    if (token) {
+      config.headers["X-XSRF-TOKEN"] = decodeURIComponent(token);
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
